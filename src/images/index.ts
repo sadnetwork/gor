2a7588e4bab5415608c6d179b416a9b2cee780c8
sad <sadnetwork@hotmail.com>
2024-07-13 19:35:25 +0700

    Create index.ts

---
+++ src/images/index.ts
@@ -0,0 +1,15 @@
+import bear from "./gor.png";
+import coin from "./gor.png";
+import highVoltage from "./high-voltage.png";
+import notcoin from "./gor2.jpg";
+import rocket from "./rocket.png";
+import trophy from "./trophy.png";
+
+export {
+    bear,
+    coin,
+    highVoltage,
+    notcoin,
+    rocket,
+    trophy
+}
